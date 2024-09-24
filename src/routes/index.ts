4fa219318fc0e8c5f35557f23029e8637e7cd162
KowkaVN <kowka25122000@gmail.com>
2024-09-24 22:19:34 +0300

    Открыл все роуты

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,17 @@
 import { Express } from "express";
 import notificationApi from "./notifications";
-// import commentsApi from "./comments";
-// import articlesApi from "./artciles";
-// import usersApi from "./users";
-// import profilesApi from "./profiles";
-// import articleRatingsApi from "./article_ratings";
+import commentsApi from "./comments";
+import articlesApi from "./artciles";
+import usersApi from "./users";
+import profilesApi from "./profiles";
+import articleRatingsApi from "./article_ratings";
 
 export const setupRoutes = (app: Express) => {
   app.use("/notifications", notificationApi);
-  // app.use("/articles", articlesApi);
-  // app.use("/comments", commentsApi);
-  // app.use("/users", usersApi);
-  // app.use("/profile", profilesApi);
-  // app.use("/article-ratings", articleRatingsApi);
-  // app.use("/profile-ratings", notificationApi);
+  app.use("/articles", articlesApi);
+  app.use("/comments", commentsApi);
+  app.use("/users", usersApi);
+  app.use("/profile", profilesApi);
+  app.use("/article-ratings", articleRatingsApi);
+  app.use("/profile-ratings", notificationApi);
 };
